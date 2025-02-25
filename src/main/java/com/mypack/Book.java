669972a81a4856d7285ed7ea6f73d640ad00c109
RAHUL DESHMUKH <rahul.deshmukh@configserverllp.com>
2025-02-25 17:02:36 +0530

    Replace name bprice with bvalue

--- src/main/java/com/mypack/Book.java
+++ src/main/java/com/mypack/Book.java
@@ -3,7 +3,7 @@ package com.mypack;
 public class Book {
  int bid;
  String bname,bauthor,bcategory;
- float bprice;
+ float bvalue;
 public int getBid() {
 	return bid;
 }
@@ -29,15 +29,15 @@ public void setBcategory(String bcategory) {
 	this.bcategory = bcategory;
 }
 public float getBprice() {
-	return bprice;
+	return bvalue;
 }
 public void setBprice(float bprice) {
-	this.bprice = bprice;
+	this.bvalue = bprice;
 }
 @Override
 public String toString() {
 	return "Book [bid=" + bid + ", bname=" + bname + ", bauthor=" + bauthor + ", bcategory=" + bcategory + ", bprice="
-			+ bprice + "]";
+			+ bvalue + "]";
 }
  
 
